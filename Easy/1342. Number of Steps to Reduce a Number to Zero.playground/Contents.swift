06ba50401bac92cf8446f08202ecfe34cd23f047
Leonardo Durazo <vn0h1ou@mx.wal-mart.com>
2021-03-20 19:56:17 -0700

    1342 Solved

--- Easy/1342. Number of Steps to Reduce a Number to Zero.playground/Contents.swift
+++ Easy/1342. Number of Steps to Reduce a Number to Zero.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+// MARK: 1342. Number of Steps to Reduce a Number to Zero
 
-var str = "Hello, playground"
+/// Runtime: 0 ms, faster than 100.00% of Swift online submissions for Number of Steps to Reduce a Number to Zero.
+/// Memory Usage: 13.8 MB, less than 66.93% of Swift online submissions for Number of Steps to Reduce a Number to Zero.
+
+func numberOfSteps (_ num: Int) -> Int {
+    var num = num {
+        didSet {
+            numberOfSteps += 1
+        }
+    }
+    var numberOfSteps = 0
+    while num >= 1 {
+        if num % 2 != 0 {
+            num -= 1
+        }
+        if num == 0 {
+            return numberOfSteps
+        }
+        num = num / 2
+    }
+
+    return numberOfSteps
+}
