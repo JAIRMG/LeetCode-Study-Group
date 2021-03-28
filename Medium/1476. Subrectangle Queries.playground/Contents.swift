2315dab92223e69aeed9ec48254682feb6d02dc8
Leonardo Durazo <vn0h1ou@mx.wal-mart.com>
2021-03-28 13:47:33 -0700

    Problem solved

--- Medium/1476. Subrectangle Queries.playground/Contents.swift
+++ Medium/1476. Subrectangle Queries.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+// MARK: 1476. Subrectangle Queries
+/// https://leetcode.com/problems/subrectangle-queries/submissions/
 
-var str = "Hello, playground"
+/// Runtime: 444 ms, faster than 71.21% of Swift online submissions for Subrectangle Queries.
+/// Memory Usage: 15.1 MB, less than 62.12% of Swift online submissions for Subrectangle Queries.
+
+class SubrectangleQueries {
+    var rectangle: [[Int]]
+    init(_ rectangle: [[Int]]) {
+        self.rectangle = rectangle
+    }
+
+    func updateSubrectangle(_ row1: Int, _ col1: Int, _ row2: Int, _ col2: Int, _ newValue: Int) {
+        for i in row1...row2 {
+            for j in col1...col2 {
+                rectangle[i][j] = newValue
+            }
+        }
+    }
+
+    func getValue(_ row: Int, _ col: Int) -> Int {
+        rectangle[row][col]
+    }
+}
